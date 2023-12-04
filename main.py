7a0a4888e48c13f1c31f9e251a29577bc64163eb
IsmaPi <130571151+IsmaPi@users.noreply.github.com>
2023-12-04 17:32:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,28 +3,28 @@ from players import Cat, Mouse, bfs
 import random
 
 
-def is_distance_sufficient(graph, start, end, min_distance=3):    #Best O(1) Worst O(N + M) where N is the nodes and M is the connections Average O(N + M)
+def is_min_distance_sufficient(graph, start, end, min_distance=3):    #Worst O(N + M) where N is the nodes and M is the connections Average O(N + M)
     path = bfs(graph, start, end)
     return len(path) >= min_distance
 
 
-def set_initial_positions(graph):    #Best O(1) assuming best case of the previous function Worst O(N + M)* K where N is the nodes, M is the connections, and K is how many times the loop has to iterate through creating new pairs of starting locations Average O(N + M)* K where k in the average number of iterations
-    all_positions = list(graph.vertices.keys())
-    position_cat = random.choice(all_positions)
-    all_positions.remove(position_cat)
-    position_mouse = random.choice(all_positions)
+def set_starting_positions(graph):    #Worst O(N + M)* K where N is the nodes, M is the connections, and K is how many times the loop has to iterate through creating new pairs of starting locations Average O(N + M)* K where k in the average number of iterations
+    positions = list(graph.vertices.keys())
+    position_cat = random.choice(positions)
+    positions.remove(position_cat)
+    position_mouse = random.choice(positions)
 
-    while not is_distance_sufficient(graph, position_cat, position_mouse):
-        all_positions.append(position_mouse)
-        position_mouse = random.choice(all_positions)
-        all_positions.remove(position_mouse)
+    while not is_min_distance_sufficient(graph, position_cat, position_mouse):
+        positions.append(position_mouse)
+        position_mouse = random.choice(positions)
+        positions.remove(position_mouse)
 
     return position_cat, position_mouse
 
 
-def main():    #O(moves * (N + M + P)) Where moves are the number of moves, N is the number of nodes, M is the number of connections, and P is the number of adjacent nodes from the node the player is in.
+def main():    #Worst: O(moves * (N + M + P)) Where moves are the number of moves, N is the number of nodes, M is the number of connections, and P is the number of adjacent nodes from the node the player is in. Average: O(moves * (N + M + P))
     graph = Graph()
-    graph.add_edges(    #Best O(N) Worst O(N) Average O(N) where N is the nodes
+    graph.add_edges(    #Worst O(N) Average O(N) Where N is the number of nodes
         (1, 2), (2, 3), (3, 4),
         (4, 5), (5, 6), (6, 7),
         (7, 8), (8, 1), (1, 5),
@@ -37,18 +37,18 @@ def main():    #O(moves * (N + M + P)) Where moves are the number of moves, N is
         (6, 14), (7, 15), (8, 9)
     )
 
-    position_cat, position_mouse = set_initial_positions(graph)    #Same time complexity as in the definition
+    position_cat, position_mouse = set_starting_positions(graph)
     cat = Cat(position_cat)
     mouse = Mouse(position_mouse)
 
-    moves_limit = 10
-    for moves_num in range(1, moves_limit + 1):
-        print(f"\nMove: {moves_num}")
+    moves= 10
+    for move in range(1, moves + 1):
+        print(f"\nMove: {moves}")
 
-        cat.move_towards_mouse(mouse, graph)    #Best: O(1) Worst: O(M*N) where M is the number of nodes and N is the number of connections Average: Depending on the size of the graph, but it would be between O(1) and O(M*N)
-
+        cat.move_towards_mouse(mouse, graph)
+        
         if mouse.eaten:
-            print("The cat has caught the mouse! The cat wins!")
+            print("You have been eaten by the cat! You better be more careful next time if you want to win.")
             break
 
         print("\nMouse's turn:")
@@ -60,19 +60,19 @@ def main():    #O(moves * (N + M + P)) Where moves are the number of moves, N is
             try:
                 next_position = int(input("Enter where you want to move next: "))
                 if next_position in graph.vertices[mouse.position]:
-                    mouse.move_mouse(next_position, graph)     #Best: O(1) Worst: O(N) Average: O(N)
+                    mouse.move_mouse(next_position, graph)
                     valid_move = True
                 else:
-                    print("Invalid move. Please choose a valid adjacent vertex.")
+                    print("Sorry, but the move you have tried is not possible, perhaps you should move to one of the positions which are accessible from where you are right now.")
             except ValueError:
-                print("Invalid input. Please enter a number.")
+                print("Sorry, but your input is not the number of a node. Please try again.")
 
         if mouse.eaten:
             print("The cat has caught the mouse! The cat wins!")
             break
 
-        if moves_num == moves_limit:
-            print("Move limit reached. The mouse wins!")
+        if moves == move:
+            print("You have managed to defeat the cat! You live to fight another day.")
             break
 
     print("Game over.")
